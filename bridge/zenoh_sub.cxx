a133d47099ab2c5d923cc88088f2930944654f64
ChenYing Kuo <evshary@gmail.com>
2025-07-29 13:33:56 +0800

    Add comments.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- bridge/zenoh_sub.cxx
+++ bridge/zenoh_sub.cxx
@@ -21,24 +21,31 @@ using namespace std::chrono_literals;
 int main(int argc, char **argv) {
     std::cout << "Zenoh Subscriber Example" << std::endl;
 
+    // Initialize Zenoh session
     zenoh::Config config = zenoh::Config::create_default();
     auto session = Session::open(std::move(config));
 
+    // Subscribe to /tf
     KeyExpr tf_keyexpr(ROS_TOPIC_TF);
     auto tf_handler = [](const Sample &sample) {
         std::cout << ">> [TF Subscriber] Received" << std::endl;
+        // TODO(CY): Deserialize the TF message and process it
     }; 
     auto tf_subscriber = session.declare_subscriber(tf_keyexpr, tf_handler, closures::none);
 
+    // Subscribe to /point_cloud
     KeyExpr point_cloud_keyexpr(ROS_TOPIC_POINT_CLOUD);
     auto point_cloud_handler = [](const Sample &sample) {
         std::cout << ">> [Point Cloud Subscriber] Received" << std::endl;
+        // TODO(CY): Deserialize the Point Cloud message and process it
     }; 
     auto point_cloud_subscriber = session.declare_subscriber(point_cloud_keyexpr, point_cloud_handler, closures::none);
 
+    // Waiting for CTRL-C to exit
     std::cout << "Press CTRL-C to quit...\n";
     while (true) {
-        std::this_thread::sleep_for(1s);
+        std::this_thread::sleep_for(5s);
+        // TODO(CY): Send query to /tf_static
     }
 
     return 0;
